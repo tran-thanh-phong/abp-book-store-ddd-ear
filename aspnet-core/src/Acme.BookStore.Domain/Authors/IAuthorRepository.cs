ae3104ad0522981c6c0d57b8e76c911a9db90de3
Tran Thanh Phong <tranthanhphong91@gmail.com>
2023-07-22 17:04:23 +0700

    Part 6: Authors: Domain layer - IAuthorRepository

--- aspnet-core/src/Acme.BookStore.Domain/Authors/IAuthorRepository.cs
+++ aspnet-core/src/Acme.BookStore.Domain/Authors/IAuthorRepository.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using Volo.Abp.Domain.Repositories;
 
-namespace Acme.BookStore.Authors
+namespace Acme.BookStore.Authors;
+
+public interface IAuthorRepository : IRepository<Author, Guid>
 {
-    internal class IAuthorRepository
-    {
-    }
+    Task<Author> FindByNameAsync(string name);
+
+    Task<List<Author>> GetListAsync(
+        int skipCount,
+        int maxResultCount,
+        string sorting,
+        string filter = null
+    );
 }
